8cd174a982486ea01c180a1b9eb58e5c80d3b369
Dylan Alpiger <dylanalpiger@bellsouth.net>
2016-10-23 17:55:55 -0400

    Python and HTML update
    I made some additions to the Python code. It now functions very
    similarly to our JavaScript code (without the HTML hookin yet). The
    script now successfully collects all user data, project data, and update
    data; and organizes that data throughout a series of logical objects.
    Note: the code takes a while to run!
    
    Also, I added a very simple and quick message to the bottom of the
    webpage asking for user feedback. This message can (and should) be
    adjusted with ease.

--- Python code/Giddy TV/script.py
+++ Python code/Giddy TV/script.py
@@ -5,7 +5,6 @@ Created on Oct 19, 2016
 '''
 import json
 import requests
-from _overlapped import NULL
 
 class User:
     def __init__(self):
@@ -88,10 +87,7 @@ class Update:
         self.name = ''
         self.detail = ''
         self.images = []
-        
-    def addImage(self, code):
-        self.images.append(code)
-        
+    
     def getName(self):
         return self.name
         
@@ -106,18 +102,71 @@ class Update:
         
     def setDetail(self, detail):
         self.detail = detail
-      
-      
+        
+    def setImages(self, imageList):
+        self.images = imageList
+        
+    def setImage(self, i, string):
+        self.images[i] = string
+
+class Envoy:
+    def __init__(self):
+        self.name = ''
+        self.username = ''
+        self.email = ''
+        self.signInTime = ''
+        self.signOutTime = ''
+        
+    def getName(self):
+        return self.name
+    
+    def getUsername(self):
+        return self.username
+    
+    def getEmail(self):
+        return self.email
+    
+    def getSignInTime(self):
+        return self.signInTime
+    
+    def getSignOutTime(self):
+        return self.signOutTime
+    
+    def setName(self, n):
+        self.name = n
+        
+    def setUsername(self, u):
+        self.username = u
+        
+    def setEmail(self, e):
+        self.email = e
+        
+    def setSignInTime(self, i):
+        self.signInTime = i
+        
+    def setSignOutTime(self, o):
+        self.signOutTime = o
+
+#Perform GET request      
+def GET(a):
+    r = requests.get(a)
+    r.json()
+    return json.loads(r.text)
+
+#Find specific project within allProjects by it's name
+def findProj(projList, name):
+    for i in range(len(projList)):
+        if projList[i].getName() == name:
+            return projList[i]
       
 #---MAIN---#
 allUsers = []
 allProjects = []
-API = 'https://firstbuild-stg.herokuapp.com/v1/users'
+GiddyAPI = 'https://firstbuild-stg.herokuapp.com/v1/users'
+EnvoyAPI = 'https://app.envoy.com/api/entries.json?api_key=db8ec594e512921a33729ffd0b7df1e1'
 
 #Fill list of users
-r = requests.get(API)
-r.json()
-data = json.loads(r.text)
+data = GET(GiddyAPI)
 for i in range(len(data['users'])):
     allUsers.append(User())
     if 'name' in data['users'][i].keys():
@@ -126,34 +175,124 @@ for i in range(len(data['users'])):
         allUsers[i].setUsername(data['users'][i]['username'])
     if 'avatar' in data['users'][i].keys():
         allUsers[i].setImage(data['users'][i]['avatar'])
+        if (allUsers[i].getImage()[-4:] != '/raw' and allUsers[i].getImage() is not None and allUsers[i].getImage() != ''):
+            allUsers[i].setImage(allUsers[i].getImage() + '/raw')
     
 #Fill users with their projects
 for i in range(len(allUsers)):
-    s = API + '/' + allUsers[i].getUsername() + '/prototypes'
+    s = GiddyAPI + '/' + allUsers[i].getUsername() + '/prototypes'
     #print (s)
-    r = requests.get(s)
-    r.json()
-    data = json.loads(r.text)
+    data = GET(s)
     for j in range(len(data['prototypes'])):
-        allProjects.append(Project())
-        
-        allUsers[i].addProject(allProjects[j])
-        allProjects[j].addUser(allUsers[i])
-        
+        proj = Project()
         if 'name' in data['prototypes'][j].keys():
-            allProjects[j].setName(data['prototypes'][j]['name'])
+            proj.setName(data['prototypes'][j]['name'])
         if 'title' in data['prototypes'][j].keys():
-            allProjects[j].setTitle(data['prototypes'][j]['title'])
+            proj.setTitle(data['prototypes'][j]['title'])
         if 'description' in data['prototypes'][j].keys():
-            allProjects[j].setDetail(data['prototypes'][j]['description'])
+            proj.setDetail(data['prototypes'][j]['description'])
         if 'image' in data['prototypes'][j].keys():
-            allProjects[j].setImage(data['prototypes'][j]['image'])
-
-        print (allUsers[i].getUsername(), ":", allUsers[i].getProject(j).getTitle())
+            proj.setImage(data['prototypes'][j]['image'])
+            if (proj.getImage() is not None and proj.getImage() != ''):
+                if proj.getImage()[-4:] != '/raw':
+                    proj.setImage(proj.getImage() + '/raw')
+                
+                #if the project image is an mp4, blank the image URL
+                dahtah = GET(proj.getImage()[:-4])
+                if dahtah['extension'] == 'mp4':
+                    proj.setImage(None)
         
-#for i in range(len(allProjects)):
-    #print (allProjects[i].getTitle())
+        #if the project has already been acounted for, simply add the user to the project's team and the project to the user's list of projects
+        thisProj = findProj(allProjects, proj.getName())
+        if thisProj is not None:
+            allUsers[i].addProject(thisProj)
+            thisProj.addUser(allUsers[i])
+        
+        #otherwise add the new project to the user's list, add the user to the new project's team, collect all updates for the new project, and add the new project to the list of allProjects
+        else:
+            #collect all updates for this new project
+            u = s + '/' + proj.getName() + '/updates'
+            d = GET(u)
+            for k in range(len(d['updates'])):
+                update = Update()
+                if 'description' in d['updates'][k].keys():
+                    update.setDetail(d['updates'][k]['description'])
+                if 'title' in d['updates'][k].keys():
+                    update.setName(d['updates'][k]['title'])
+                if 'uploads' in d['updates'][k].keys():
+                    update.setImages(d['updates'][k]['uploads'])
+                    for l in range(len(update.images)):
+                        if (update.getImage(l) is not None and update.getImage(l) != ''):
+                            update.setImage(l, GiddyAPI + '/' + allUsers[i].getUsername() + '/uploads/' + update.getImage(l) + '/raw')
+                        
+                            #if the update image is an mp4, blank the image URL
+                            dahtah = GET(update.getImage(l)[:-4])
+                            if 'extension' in dahtah.keys():
+                                if dahtah['extension'] == 'mp4':
+                                    update.setImage(l,None)
+                    
+                proj.addUpdate(update)
+            
+            allUsers[i].addProject(proj)
+            proj.addUser(allUsers[i])
+            allProjects.append(proj)
 
+#print information for the first user
+print ("Username:", allUsers[0].getUsername())
+print ("Name:", allUsers[0].getName())
+print ("Avatar:", allUsers[0].getImage())
+print ("---User's Projects---")
+for i in range(len(allUsers[0].projects)):
+    print ("\tName:", allUsers[0].getProject(i).getName())
+    print ("\tTitle:", allUsers[0].getProject(i).getTitle())
+    try:
+        print ("\tDescription:", allUsers[0].getProject(i).getDetail())
+    except:
+        print ("\t\tDescription: Description has emojis!")
+    print ("\t---Team---")
+    for j in range(len(allUsers[0].getProject(i).team)):
+        print ("\tUsername:", allUsers[0].getProject(i).getMember(j))
+    print ("\t---Updates---")
+    for k in range(len(allUsers[0].getProject(i).updates)):
+        try:
+            print ("\t\tName:", allUsers[0].getProject(i).getUpdate(k).getName())
+        except:
+            print ("\t\tName: Name has emojis!")
+        try:
+            print ("\t\tDescription:", allUsers[0].getProject(i).getUpdate(k).getDetail())
+        except:
+            print ("\t\tDescription: Description has emojis!")
+        print ("\t\tImages:", allUsers[0].getProject(i).getUpdate(k).images, "\n")
+
+#print every project and the full team of that project    
+#for i in range(len(allProjects)):
+#    for j in range(len(allProjects[i].team)):
+#        print (allProjects[i].getTitle(), ":", allProjects[i].getMember(j).getUsername())
+        
+#print all users and the projects associated with each user (note if the user has no projects, they will not appear)
+#for i in range(len(allUsers)):
+#    for j in range(len(allUsers[i].projects)):
+#        print (allUsers[i].getUsername(), ":", allUsers[i].getProject(j).getTitle())
+
+#Check all projects' image URLs
+#for i in range(len(allProjects)):
+#    print (allProjects[i].getTitle(), ":", allProjects[i].getImage())
+        
+#Print all updates for all projects
+#for i in range(len(allProjects)):
+#    for j in range(len(allProjects[i].updates)):
+#        try:
+#            print (allProjects[i].getTitle(), ":", allProjects[i].getUpdate(j).getName())
+#        except:
+#            print ("Update has emoji in title!")    #warning: descriptions have also shown to have emojis
+
+#Print all updates and update images for all projects
+#for i in range(len(allProjects)):
+#    for j in range(len(allProjects[i].updates)):
+#        try:
+#            print (allProjects[i].getTitle(), ":", allProjects[i].getUpdate(j).getName(), ":", allProjects[i].getUpdate(j).images)
+#        except:
+#            print ("Update has emoji in title! :", allProjects[i].getUpdate(j).images)    #warning: descriptions have also shown to have emojis
         
 
 
